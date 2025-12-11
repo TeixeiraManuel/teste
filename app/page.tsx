9cb17f1a63505c6f76c0ea4185628999d5cb13a5
temanuel <temanuel@student.42.fr>
2025-12-11 15:07:19 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,7 @@ const videoConstraints = {
 
 const Home = () => {
   const webcamRef = useRef<any>(null);
-  const [facingMode, setFacingMode] = useState<"user" | "environment">(
-    "environment",
-  );
-  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
-
+  const [facingMode, setFacingMode] = useState<"user">("environment");
   // Tipagem para deviceId
   const [deviceId, setDeviceId] = useState<string>("");
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
@@ -37,42 +33,9 @@ const Home = () => {
   return (
     <div className="flex flex-col h-screen items-center justify-center bg-black w-full p-20 md:p-0">
       <div>
-        <Webcam
-          ref={webcamRef}
-          audio={false}
-          height={360}
-          screenshotFormat="image/jpeg"
-          width={720}
-          videoConstraints={{
-            ...videoConstraints,
-            facingMode,
-          }}
-        />
-      </div>
-
-      <div className="flex">
-        {recordedChunks.length > 0 && (
-          <video
-            controls
-            style={{
-              marginTop: 20,
-              width: 720,
-              height: 360,
-            }}
-          >
-            <source
-              src={URL.createObjectURL(
-                new Blob(recordedChunks, { type: "video/webm" }),
-              )}
-              type="video/webm"
-            />
-          </video>
-        )}
+        <Webcam ref={webcamRef} audio={false} height={360} width={720} />
       </div>
       <div className="text-white">
-        {/* {devices.map((device, key) => (
-          <div>{device.label || `Device ${key + 1}`}</div>
-        ))} */}
         <select
           value={deviceId}
           onChange={(e) => setDeviceId(e.target.value)}
